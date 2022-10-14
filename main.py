c5e6728c5c25bce57b2899a7d046825bf03cacde
drissahm <driss.ahmidouch@gmail.com>
2022-10-14 17:19:39 +0100

    temp table fix

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import pandas as pd
 def hello_gcs(event, context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
-    event (dict): Event payload.
-    context (google.cloud.functions.Context): Metadata for the event.
+      event (dict): Event payload.
+      context (google.cloud.functions.Context): Metadata for the event.
     """
     # it is mandatory initialize the storage client
     client_st = storage.Client()
@@ -24,20 +24,16 @@ def hello_gcs(event, context):
     df['CREDIT'] = df['CREDIT'].astype('str').str.replace(r',', '.')
     df['DEBIT'] = df['DEBIT'].astype('float')
     df['CREDIT'] = df['CREDIT'].astype('float')
-    df['DATE_OPERATION'] = df['DATE_OPERATION'].astype('datetime64[ns]').dt.date
-    df['DATE_VALEUR'] = df['DATE_VALEUR'].astype('datetime64[ns]').dt.date
+    df['DATE_OPERATION'] = pd.to_datetime(df['DATE_OPERATION'], format='%d/%m/%Y %H:%M:%S')
+    df['DATE_VALEUR'] = pd.to_datetime(df['DATE_VALEUR'], format='%d/%m/%Y %H:%M:%S')
 
     # define the schema for the bigquery table
     my_schema = [{'name': 'DATE_OPERATION', 'type': 'DATE'}, {'name': 'DATE_VALEUR', 'type': 'DATE'}, {'name': 'LIBELLE', 'type': 'STRING'},{'name': 'DEBIT', 'type': 'FLOAT'},{'name': 'CREDIT', 'type': 'FLOAT'}]
     # upload the dataframe to big query
-    df.to_gbq('cih_monitoring.bank_statement_raw_temp', 'ma-cl-cih', chunksize=10000, if_exists='replace', table_schema=my_schema)
+    df.to_gbq('cih_monitoring.bank_statement_import', 'ma-cl-cih', chunksize=10000, if_exists='replace', table_schema=my_schema)
 
-    # insert the temp table to the permanent table
-    insert_command = """INSERT INTO cih_monitoring.bank_statement_raw 
-    SELECT * FROM cih_monitoring.bank_statement_raw_temp;"""
+    # insert the imported data into the clean table while removing duplicates
+    insert_command = """CREATE OR REPLACE TABLE cih_monitoring.bank_statement_clean AS
+    (SELECT * FROM cih_monitoring.bank_statement_clean) UNION DISTINCT
+    (SELECT * FROM cih_monitoring.bank_statement_import);"""
     query_job = client_bq.query(insert_command)
-
-    # drop duplicates of the permanent table
-    drop_duplicates_command = """CREATE OR REPLACE TABLE cih_monitoring.bank_statement_raw
-    AS SELECT DISTINCT * FROM cih_monitoring.bank_statement_raw;"""
-    query_job_2 = client_bq.query(drop_duplicates_command)
